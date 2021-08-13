5a3e767c4f40470d33153d619b5dee92ae7587ee
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-08-13 09:50:21 +0300

    last commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 
-var countDownDate = new Date("Aug 15, 2021 00:00:00").getTime();
+var countDownDate = new Date("Aug 17, 2021 00:00:00").getTime();
 
 var x = setInterval(function() {
 
@@ -23,3 +23,26 @@ var x = setInterval(function() {
 
 
 }, 1000);
+
+
+var slideImg = document.getElementById("slideImg");
+
+var images = new Array(
+
+    "images/logo.png",
+    "images/amis4.jpg",
+    "images/woman-570883_1920.jpg"
+   
+);
+
+var len = images.length;
+var i = 0;
+
+function slider(){
+    if(i > len-1){
+        i = 0;
+    }
+    slideImg.src = images[i];
+    i++;
+    setTimeout('slider()', 3000);
+}
